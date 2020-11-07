d06fb8147bcfbf4693078892e00cf9ee44eb65c8
Paulo Henrique Coelho Gaia <paulo.coelho@webjump.com.br>
2020-11-07 12:35:21 -0300

    Updated knex file

--- knexfile.ts
+++ knexfile.ts
@@ -9,6 +9,8 @@ module.exports = {
   development: {
     client: process.env.DB_CONNECTION,
     connection: {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
       database: process.env.DB_DATABASE,
       user: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD
